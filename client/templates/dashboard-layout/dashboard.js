48ca0c44956b3c643268ae6904652799fe3aebd6
ErikRamses <erik.rmh@gmail.com>
2017-03-16 12:37:00 -0600

    loginHeader
    Login y header

--- client/templates/dashboard-layout/dashboard.js
+++ client/templates/dashboard-layout/dashboard.js
@@ -1,7 +1,3 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
 /*
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
