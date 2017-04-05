c764c04f81f8470bf554cef204f32cee402fd5da
ErikRamses <erik.rmh@gmail.com>
2017-04-05 11:17:44 -0500

    OrderCreate
    Creación de órdenes por formulario en modal

--- client/templates/ordenes/ordenes.js
+++ client/templates/ordenes/ordenes.js
@@ -22,5 +22,39 @@ Template.ordenes.events({
       	Ordenes.insert({}); //actualizo mi documento con mi key, value recibidos en var info
       	//console.log(text); //compruebo los datos
       	//guardado(event.currentTarget); // función que muestra el mensaje de guardado correctamente
+    },
+    'submit form': function(event){
+        event.preventDefault();
+        var cliente = $('[name=cliente]').val();
+        var nombre = $('[name=nombre]').val();
+        var calle = $('[name=calle]').val();
+        var colonia = $('[name=colonia]').val();
+        var postalcode = $('[name=postalcode]').val();
+        var latitud = $('[name=latitud]').val();
+        var longitud = $('[name=longitud]').val();
+        var sucursal = $('[name=sucursal]').val();
+        var ciudad = $('[name=ciudad]').val();
+        $('[name=cliente]').val('');
+        $('[name=nombre]').val('');
+        $('[name=calle]').val('');
+        $('[name=colonia]').val('');
+        $('[name=latitud]').val('');
+        $('[name=longitud]').val('');
+        $('[name=ciudad]').val('');
+        var ordenes = Ordenes.insert({
+          cliente: cliente, 
+          nombre: nombre, 
+          calle: calle, 
+          colonia: colonia, 
+          postalcode: postalcode, 
+          estado: sucursal, 
+          ciudad: ciudad, 
+          latitud: latitud,
+          longitud: longitud, 
+          user: Meteor.userId(), 
+          date: new Date()
+        });
+        alert('Orden guardada correctamente.');
+        $('#newOrden').modal('hide');
     }
 });
