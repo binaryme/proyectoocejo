e51592416e2ca8f909470d512b6c32a0f67e1ac3
nubestra <nubestra@gmail.com>
2017-05-14 15:09:48 -0500

    botón de perfil
    Cambio la foto de perfil y user dashboard
    cerrar sesión

---
+++ client/templates/dashboard-layout/profile-mini-dashboard/userHeader.js
@@ -0,0 +1,37 @@
+Template.userHeader.events({
+    'click .cambiar-perfil': function(event, template) {
+      $(".fotoPerfil").click();
+    },
+   'change .fotoPerfil': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+             // handle error
+          } else {
+             // handle success depending what you need to do
+            var userId = Meteor.userId();
+            var imagesURL = {
+              'profile.image': '/cfs/files/images/' + fileObj._id
+            };
+            console.log(fileObj._id);
+            delay(function() {
+              Meteor.users.update(userId, {$set: imagesURL});
+            }, 500);
+          }
+        });
+     });
+   },
+	'click .cerrar-sesion': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+});
+
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
