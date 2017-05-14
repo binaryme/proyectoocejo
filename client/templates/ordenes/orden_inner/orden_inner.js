e6e1ac3706293ae4421adfdf47182e65f294f1ae
nubestra <nubestra@gmail.com>
2017-05-14 15:15:39 -0500

    producto inner

--- client/templates/ordenes/orden_inner/orden_inner.js
+++ client/templates/ordenes/orden_inner/orden_inner.js
@@ -1,14 +1,50 @@
 /*****************************************************************************/
 /* OrdenInner: Event Handlers */
 /*****************************************************************************/
-Template.OrdenInner.events({
+Template.ProductoAgregar.events({
+	'click input': function(event, template)
+	  	{
+	      	var checked = $(event.currentTarget).is(":checked"); //tomo el name del input, para usar como key en mi key, value
+	      	var name = "orden";
+	      	var value = Router.current().params._id; //le pone al objeto de inventario el id de la orden actual para hacer join
+	      	var info = _.object([name], [value]); //convierto mi key value en un objeto para insertar en mi base de datos
+			if (checked)  {
+				Inventario.update({_id: this._id}, {$set: info});
+			} else {
+				Inventario.update({_id: this._id}, {$set: {orden : ""} });
+			}
+			console.log(this._id);
+	  	}
 });
 
 /*****************************************************************************/
-/* OrdenInner: Helpers */
+/* ListaProductos: Helpers */
 /*****************************************************************************/
-Template.OrdenInner.helpers({
+//Template.ProductoAgregar.helpers({
+//	checked: function() {
+//		if Inventario.find({this._id, noorden: null}) {
+//			return "Checked";
+//		}
+//	}
+//});
+Template.OrdenContenido.helpers({
+  inventarioEnOrden: function() {
+    //if (Session.get("searchValue")) {
+      return Inventario.find({orden: this._id});
+    //} else {
+    //  return Reportes.find({});
+    //}
+  }
+}); 
 
+Template.ListaProductos.helpers({
+  inventario: function() {
+    //if (Session.get("searchValue")) {
+      return Inventario.find({}, { sort: [["score", "desc"]] });
+    //} else {
+    //  return Reportes.find({});
+    //}
+  }
 });
 
 /*****************************************************************************/
@@ -18,12 +54,17 @@ Template.OrdenInner.onCreated(function () {
 });
 
 Template.OrdenInner.onRendered(function () {
-	$(function(){
-    	var $select = $(".1-100");
-    	for (i=1;i<=100;i++){
-    	    $select.append($('<option></option>').val(i).html(i))
-    	}
-	});
+	Meteor.subscribe("inventario");
+	delay(function() {
+		$(function(){
+    		var $select = $(".1-100");
+    		for (i=1;i<=100;i++){
+    		    $select.append($('<option></option>').val(i).html(i))
+    		}
+		});
+    }, 1000);
+
+
 });
 
 Template.OrdenInner.onDestroyed(function () {
