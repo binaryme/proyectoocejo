0ac9e385d9e6e6dab085022c407af43ff68aed15
ErikRamses <erik.rmh@gmail.com>
2017-05-16 19:52:17 -0500

    SearchinServer
    Configuración de la búsqueda desde el servidor con Regex

--- server/main.js
+++ server/main.js
@@ -3,4 +3,26 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
 });
 
+SearchSource.defineSource('inventario', function(searchText, options) {
+  var options = {sort: {isoScore: -1}, limit: 20};
+  
+  if(searchText) {
+    var regExp = buildRegExp(searchText);
+    var selector = {Descripcion: regExp};
+    
+    return Inventario.find(selector, options).fetch();
+  } else {
+    return Inventario.find({}, options).fetch();
+  }
+});
+
+function buildRegExp(searchText) {
+  var words = searchText.trim().split(/[ \-\:]+/);
+  //console.log(words);
+  var exps = _.map(words, function(word) {
+    return "(?=.*" + word + ")";
+  });
+  var fullExp = exps.join('') + ".+";
+  return new RegExp(fullExp, "i");
+}
 
