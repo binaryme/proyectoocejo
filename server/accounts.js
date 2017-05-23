b83e851cc173ab323413b9a769f6bb1b4139f6e5
nubestra <nubestra@gmail.com>
2017-05-23 17:09:41 -0500

    crea la cuenta con sus datos rellenados

--- server/accounts.js
+++ server/accounts.js
@@ -1,15 +1,17 @@
-//Accounts.onCreateUser(function(options, user) {
-//	console.log(options);
-//  if ( Meteor.users.find().count() === 0 )
-//	 user.roles = ['Admin'];
-//  else
-//    user.roles = ['Cliente'];
-//  return user;
-//});
+Accounts.onCreateUser(function(options, user) {
+	console.log(options);
+  if ( Meteor.users.find().count() === 0 )
+	  user.roles = ['super-admin'];
+    user.profile = {
+      proveedor: 'Distribuidores especializados Oejo SA DE CV.',
+      direccion: 'Calle Privada Regio No. Exterior 108 Colonia Regio Parque Industrial Municipio Apodaca Estado Nuevo León México CP. 66633'
+    };
+    return user;
+});
 
-//if ( Meteor.users.find().count() === 0 ) {
-//  var admin = Accounts.createUser({
-//      email: 'admin@deocejo.com',
-//      password: 'lechuga'
-//  });
-//}
+if ( Meteor.users.find().count() === 0 ) {
+  var admin = Accounts.createUser({
+      email: 'admin@deocejo.com',
+      password: 'lechuga'
+  });
+}
