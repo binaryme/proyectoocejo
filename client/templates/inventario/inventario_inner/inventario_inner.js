586369d45f72d9e316f0e1f76a1af1ae38a21cf3
Ramses <erik.rmh@gmail.com>
2017-07-31 15:09:29 -0500

    Modificación de imagen de inventario

--- client/templates/inventario/inventario_inner/inventario_inner.js
+++ client/templates/inventario/inventario_inner/inventario_inner.js
@@ -1,12 +1,10 @@
-Template.InventarioInner.helpers({
-  producto: function() {
-      var id = Router.current().params._id;
-      return Inventario.find({_id: id});
-  }
-}); 
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import Images from '/lib/collections/images.js';
+
 Template.InventarioInner.events({
     'click .cambiar-foto-producto': function(event, template) {
-      $('#fileManager').modal('toggle');
+      event.preventDefault();
     },
     'click .borrar-producto': function(event, template) {
       var borrar = confirm("¿Seguro que quieres borrar este producto de la base de datos?")
@@ -19,6 +17,74 @@ Template.InventarioInner.events({
       }
     }
 });
+
+Template.InventarioInner.helpers({
+  producto: function() {
+      var id = Router.current().params._id;
+      return Inventario.find({_id: id});
+  },
+  imagenProducto: function () {
+    var producto = Inventario.findOne({_id: Router.current().params._id});
+    if(producto.Imagen)
+      return Images.findOne({_id: producto.Imagen});
+    else
+    return false;
+  }
+}); 
+
+
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload: function () {
+    return Template.instance().currentUpload.get();
+  }
+});
+
+
+Template.uploadForm.events({
+  'change #fileInput': function (e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // there was multiple files selected
+      var file = e.currentTarget.files[0];
+      if (file) {
+        var uploadInstance = Images.insert({
+          file: file,
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+
+        uploadInstance.on('start', function() {
+          template.currentUpload.set(this);
+        });
+
+        uploadInstance.on('end', function(error, fileObj) {
+          if (error) {
+            window.alert('Error during upload: ' + error.reason);
+          } else {
+            window.alert('File "' + fileObj.name + '" successfully uploaded');
+            Inventario.update(Router.current().params._id, {
+              $set: { Imagen: fileObj._id}
+            });
+          }
+          template.currentUpload.set(false);
+        });
+
+        uploadInstance.start();
+      }
+    }
+  }
+});
+
+
+//Template.InventarioInner.onRendered(function () {
+//  Uploader.render.call(this);
+//});
+
+/*
 Template.fileManager.helpers({
   images: function() {
       return Images.find({});
@@ -79,3 +145,4 @@ var delay = (function(){
     timer = setTimeout(callback, ms);
   };
 })();
+*/
